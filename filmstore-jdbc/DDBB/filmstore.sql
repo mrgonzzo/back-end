386f5f51912e6c3d19c15a3c6a79ca5d2aa9f860
formacion <formacion@172.50.8.48>
2017-05-09 16:50:51 +0200

    1

---
+++ filmstore-jdbc/DDBB/filmstore.sql
@@ -0,0 +1,22 @@
+DROP DATABASE IF EXISTS DB_FILMSTORE;
+CREATE DATABASE DB_FILMSTORE;
+USE DB_FILMSTORE;
+drop table if exists TB_DIRECTORS;
+create table TB_DIRECTORS (ID int not null AUTO_INCREMENT,
+                            NAME VARCHAR(20) not null,
+							Age int not null );
+
+							
+							
+							
+							
+							
+							
+							
+							
+							
+listado
+get
+create
+delete
+update
