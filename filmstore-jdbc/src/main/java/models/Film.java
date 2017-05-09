6181ae167a1a697123c7928a656e5b25f538069e
formacion <formacion@172.50.8.48>
2017-05-09 16:43:12 +0200

    jdbc

---
+++ filmstore-jdbc/src/main/java/models/Film.java
@@ -0,0 +1,41 @@
+package models;
+
+public class Film {
+private int id;
+private String title;
+private String category;
+private Director director;
+
+public Film(String title,String category){
+	//this.id = id;
+	this.title = title;
+	this.category = category;
+	//this.director=director;
+}
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getTitle() {
+	return title;
+}
+public void setTitle(String title) {
+	this.title = title;
+}
+public String getCategory() {
+	return category;
+}
+public void setCategory(String category) {
+	this.category = category;
+}
+public Director getDirector() {
+	return director;
+}
+public void setDirector(Director director) {
+	this.director = director;
+}
+
+
+}
