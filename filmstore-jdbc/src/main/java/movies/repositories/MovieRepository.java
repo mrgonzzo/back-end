748d9c667f8effb526ea1beaf1d08492bf161a18
formacion <formacion@172.50.8.142>
2017-05-11 16:03:39 +0200

    ok

--- filmstore-jdbc/src/main/java/movies/repositories/MovieRepository.java
+++ filmstore-jdbc/src/main/java/movies/repositories/MovieRepository.java
@@ -8,5 +8,7 @@ import movies.model.Film;
 
 public interface MovieRepository extends CrudRepository<Film, Integer>{
 	List<Film> findByCategory(String category);
-
+	/*
+	 * para querys complejas que esten mas alla del jpa
+	@Query ("query con JPSQL que es lo mismo que hql");*/
 }
